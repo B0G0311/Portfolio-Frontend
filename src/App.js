5c27083c0ec885fe1361c7d53839e7da48352aab
Thomas <ptgarland@protonmail.com>
2022-08-13 14:13:49 -0400

    configured to show backend data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    projects: []
+  };
+  
+  /*    This is where the magic happens*/  
+  
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/projects/api/'); // fetching the data from api, before the page loaded
+      const projects = await res.json();
+      this.setState({
+        projects
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.projects.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
